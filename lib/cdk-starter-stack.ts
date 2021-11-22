e11025befc075a5c2c74962951e905b969c5cc56
Enrique M. Talavera <kiquetal@pop-os.localdomain>
2021-11-21 23:49:08 -0300

    starter for lambda+dynamo

--- lib/cdk-starter-stack.ts
+++ lib/cdk-starter-stack.ts
@@ -1,14 +1,16 @@
 import * as lambda from '@aws-cdk/aws-lambda';
+import * as dynamodb from '@aws-cdk/aws-dynamodb';
 import {NodejsFunction} from '@aws-cdk/aws-lambda-nodejs';
 import * as cdk from '@aws-cdk/core';
 import * as path from 'path';
+import * as iam from '@aws-cdk/aws-iam';
 
 export class CdkStarterStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const myFunction = new NodejsFunction(this, 'my-function', {
-      memorySize: 1024,
+    const myFunction = new NodejsFunction(this, 'data-mgm-function-queries', {
+      memorySize: 768,
       timeout: cdk.Duration.seconds(5),
       runtime: lambda.Runtime.NODEJS_14_X,
       handler: 'main',
@@ -18,5 +20,15 @@ export class CdkStarterStack extends cdk.Stack {
         externalModules: ['aws-sdk'],
       },
     });
+
+    myFunction.role?.addManagedPolicy(
+        iam.ManagedPolicy.fromAwsManagedPolicyName(
+            'service-role/AWSLambdaBasicExecutionRole',
+        ),
+    );
+
+
+
+
   }
 }
